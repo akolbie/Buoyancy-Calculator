506dc9a82512d7ee3fa9708af13e05493e329824
Adam Kolb <adam.kolb9@gmail.com>
2022-04-08 08:35:48 -0400

    Added data csv, added parsing to import data csv into script

--- buoy_calc.py
+++ buoy_calc.py
@@ -7,6 +7,9 @@ Vessel COG and COB is measured from the bottom of the vessel up
 Water height is measured 
 """
 
+import csv
+from turtle import Turtle
+from unicodedata import name
 
 WATER_DENSITY = 1000 #KG/M^3
 GRAVITY = 9.81 #M/S^2
@@ -22,7 +25,8 @@ class Vessel():
     Defines any item adding contributing to the
     vehicles mass and buoyancy
     """
-    def __init__(self, mass, volume, COG, COB, height):
+    def __init__(self, name, mass, volume, COG, COB, height):
+        self.name = name
         self.weight = mass * GRAVITY
         self.buoyancy = volume * WATER_DENSITY * GRAVITY
         self.COG = COG
@@ -34,8 +38,8 @@ class SideWall(Vessel):
     Sub-class of vessel, specifically for the vertical 
     walls of the vessel, to allow smoother waterline calcs
     """
-    def __init__(self, mass, volume, COG, COB, height):
-        super().__init__(mass, volume, COG, COB, height)
+    def __init__(self, name, mass, volume, COG, COB, height):
+        super().__init__(name, mass, volume, COG, COB, height)
 
     def buoyancy_at_point(self, position):
         if position > self.height:
@@ -154,23 +158,45 @@ class Vessel_Comparison():
         self.full_vehicle.recalc()
         return amount, self.empty_vehicle.vessels[-1].weight # maybe just pass the vessel?
 
-def import_vessels(location):
+def split_csv_row(row):
+    return [
+        row[0],
+        *row[2:7],
+        row[7:]
+    ]
+
+def import_data(location):
     """
     Returns a list of vessel objects
     """
-    pass
-
-def import_heights(location):
-    """
-    Returns a list of list of heights of corresponding vessels
-    """
-    pass
-
-def import_vehicle_data(location):
-    """
-    Returns a dictionary of vehicle data
-    """
-    pass
+    vessel = []
+    walls = []
+    varying_vessel = []
+    
+    vessel_flag = True
+    vehicle_height_flag = False
+    with open(location, "r") as f:
+        csv_reader = csv.reader(f)
+        for i, row in csv_reader:
+            if i == 0:
+                continue
+            elif row[0] == "0":
+                vessel_flag = False
+                continue
+            elif vessel_flag:
+                if row[1] == "Module":
+                    vessel.append(split_csv_row(row))
+                elif row[1] == "Wall":
+                    walls.append(split_csv_row(row))
+                else:
+                    varying_vessel.append(split_csv_row(row))
+            else:
+                if not vehicle_height_flag:
+                    vehicle_height = row[1]
+                    vehicle_height_flag = True
+                else:
+                    water_height = row[1]
+    return vessel, walls, varying_vessel, vehicle_height, water_height
 
 def split_vessels(vessels):
     return vessels[:-2], vessels[-2], vessels[-1]
@@ -207,9 +233,7 @@ def vehicle_comparisons(vessels, heights, vehicle_data):
 
 
 if __name__ == '__main__':
-    vessels = import_vessels('vessels.csv')
-    heights = import_heights('heights.csv')
-    vehicle_data = import_vehicle_data('vehicle_data.csv')
+    vessels, walls, varying_vessels, vehicle_height, water_height = import_data('data.csv')
 
     #multiple_vehicles(vessels, heights, vehicle_data)
     #vehicle_comparisons(vessels, heights, vehicle_data)
